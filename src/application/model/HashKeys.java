c4586c23119208941280ded49457326779bf7132
Alex White <alex.whie@oval.uk.com>
2015-10-16 15:35:09 +0100

    added hashkey enum for use with hashmaps and database queries

---
+++ src/application/model/HashKeys.java
@@ -0,0 +1,41 @@
+package application.model;
+
+public enum HashKeys
+{
+	HOST( "host", 0 ),
+	USERNAME( "username", 1 ),
+	PASSWORD( "password", 2 ),
+	PATH( "path", 3 );
+	
+	
+	private String name;
+	private int ordinal;
+	
+	HashKeys( String name, int ordinal )
+	{
+		this.name = name;
+		this.ordinal = ordinal;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * @return the ordinal
+	 */
+	public int getOrdinal()
+	{
+		return ordinal;
+	}
+	
+	@Override
+	public String toString()
+	{
+		return name;
+	}
+}
